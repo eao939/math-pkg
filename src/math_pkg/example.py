07555254a43c04067556c6535eb9c1aa73d17f45
EAO939 <A.EUNA939@GMAIL.COM>
2026-04-04 16:50:54 -0700

    created math-pkg. up to locally installing package

---
+++ src/math_pkg/example.py
@@ -0,0 +1,87 @@
+"""
+Module containing basic math operations 
+"""
+
+def add_numbers(a, b):
+    """
+    Add two numbers together and return the result.
+
+    Parameters
+    ----------
+    a : float
+        The first number to add.
+    b : float
+        The second number to add.
+
+    Returns
+    -------
+    float
+        The sum of the two numbers.
+
+    Examples
+    --------
+    >>> add_numbers(3, 5)
+    8
+    >>> add_numbers(-2, 7)
+    5
+
+    """
+    return a + b
+
+
+
+
+def subtract_numbers(a, b):
+    """
+    Subtracts the second number from the first and returns the result.
+
+    Parameters
+    ----------
+    a : float
+        The first number to subtract from.
+    b : float
+        The second number to subtract.
+
+    Returns
+    -------
+    float
+        The difference of the two numbers.
+
+    Examples
+    --------
+    >>> subtract_numbers(13, 5)
+    8
+    >>> add_numbers(-2, 7)
+    -9
+
+    """
+    return a - b
+
+
+
+
+
+def square_number(a):
+    """
+    Squares a nunmber and returns the result.
+
+    Parameters
+    ----------
+    a : float
+        The number to square
+
+    Returns
+    -------
+    float
+        The number squared
+
+    Examples
+    --------
+    >>> subtract_numbers(9)
+    81
+    >>> add_numbers(-7)
+    49
+
+    """
+    return a**2
+
